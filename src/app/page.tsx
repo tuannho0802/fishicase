74a953d8e82edb83c41df64cef9c438a6f273d5c
HoangTuanFT <hoangtuanft0802@gmail.com>
2024-07-25 09:43:30 +0700

    Add footer and other details in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import { Icons } from "@/components/Icon";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  ArrowRight,
+  Check,
+  Star,
+} from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,21 +25,31 @@ export default function Home() {
             flex flex-col items-center lg:items-start"
             >
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/fishi-1.png" className="w-full" alt="logo" />
+                <img
+                  src="/fishi-1.png"
+                  className="w-full"
+                  alt="logo"
+                />
               </div>
               <h1
                 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight 
               text-gray-900 text-5xl md:text-6xl lg:text-7xl"
               >
                 Your Images is on a{" "}
-                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                <span className="bg-green-600 px-2 text-white">
+                  Custom
+                </span>{" "}
                 Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
-                Capture your favorite memories with your own,
-                <span className="font-semibold">one-of-one </span>
-                phone case. FishiCasee allows you to protect your memories, not
-                just your phone case.
+                Capture your favorite memories
+                with your own,
+                <span className="font-semibold">
+                  one-of-one{" "}
+                </span>
+                phone case. FishiCasee allows
+                you to protect your memories,
+                not just your phone case.
               </p>
               <ul
                 className="mt-8 space-y-2 text-left font-medium flex
@@ -42,15 +58,17 @@ export default function Home() {
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
-                    High-quality, durable material
+                    High-quality, durable
+                    material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 years
-                    5 years guarantee
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    5 years 5 years guarantee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 years
-                    Modern iPhone models supported
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    5 years Modern iPhone models
+                    supported
                   </li>
                 </div>
               </ul>
@@ -94,7 +112,10 @@ export default function Home() {
                   </div>
 
                   <p>
-                    <span className="font-semibold">1.250</span> happy customers
+                    <span className="font-semibold">
+                      1.250
+                    </span>{" "}
+                    happy customers
                   </p>
                 </div>
               </div>
@@ -113,7 +134,10 @@ export default function Home() {
                 className="absolute w-20 -left-6 -bottom-6 select-none"
                 alt="line image"
               />
-              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+              <Phone
+                className="w-64"
+                imgSrc="/testimonials/1.jpg"
+              />
             </div>
           </div>
         </MaxWidthWrapper>
@@ -150,13 +174,17 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  &quot;The case feels durable and I even got a compliment on
-                  the design. Had the case for two and a half month now and{" "}
+                  &quot;The case feels durable
+                  and I even got a compliment on
+                  the design. Had the case for
+                  two and a half month now and{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
                     the image is super clear
                   </span>
-                  , on the case I had before, the image started fading into
-                  yellow-ish color after a couple weeks. Love it!!&quot;
+                  , on the case I had before,
+                  the image started fading into
+                  yellow-ish color after a
+                  couple weeks. Love it!!&quot;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -166,10 +194,14 @@ export default function Home() {
                   alt="user"
                 />
                 <div className="flex flex-col">
-                  <p className="font-semibold">Jonathan</p>
+                  <p className="font-semibold">
+                    Jonathan
+                  </p>
                   <div className="flex gap-1 items-center text-zinc-600">
                     <Check className="h-4 w-4 stroke-[3px] text-green-600" />{" "}
-                    <p className="text-sm">Verified Purchase</p>
+                    <p className="text-sm">
+                      Verified Purchase
+                    </p>
                   </div>
                 </div>
               </div>
@@ -188,12 +220,17 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  &quot; I usually keep my phone together with my keys in my
-                  pocket and that led to some pretty heavy scratchmarks on all
-                  of my last phone cases. This one, besides a barely noticeable
-                  scratch on the corner,{" "}
+                  &quot; I usually keep my phone
+                  together with my keys in my
+                  pocket and that led to some
+                  pretty heavy scratchmarks on
+                  all of my last phone cases.
+                  This one, besides a barely
+                  noticeable scratch on the
+                  corner,{" "}
                   <span className="p-0.5 bg-slate-800 text-white">
-                    looks brand new after about half a year
+                    looks brand new after about
+                    half a year
                   </span>
                   . I dig it! &quot;
                 </p>
@@ -205,10 +242,14 @@ export default function Home() {
                   alt="user"
                 />
                 <div className="flex flex-col">
-                  <p className="font-semibold">Jeff</p>
+                  <p className="font-semibold">
+                    Jeff
+                  </p>
                   <div className="flex gap-1 items-center text-zinc-600">
                     <Check className="h-4 w-4 stroke-[3px] text-green-600" />{" "}
-                    <p className="text-sm">Verified Purchase</p>
+                    <p className="text-sm">
+                      Verified Purchase
+                    </p>
                   </div>
                 </div>
               </div>
@@ -219,6 +260,75 @@ export default function Home() {
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-green-600 text-white">
+                  your own case
+                </span>{" "}
+                now!
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                alt="arrow image"
+              />
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                  alt="horse.jpg"
+                />
+              </div>
+              <Phone
+                className="w-60"
+                imgSrc="/horse_phone.jpg"
+              />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-2-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              High-quality silicon material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Scratch and fingerprint resistant
+              coating
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-green-600 inline mr-1.5" />
+              5 years print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link className={buttonVariants(
+                {
+                  size:"lg",
+                  className:"mx-auto mt-8"
+                }
+              )} href="/configure/upload">
+                Create your case now{" "}
+                <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
